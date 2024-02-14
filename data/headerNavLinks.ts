d213748dadfe3ef9cf7fb9b2c26cf4a0a762f1ee
enlytaibot <156079526+enlytaibot@users.noreply.github.com>
2024-02-14 15:36:47 +0545

    Update header navigation links

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -4,6 +4,7 @@ const headerNavLinks = [
   { href: '/tags', title: 'Tags' },
   { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
+  { href: '/login', title: 'Login' }, // Added new login link
 ]
 
 export default headerNavLinks
